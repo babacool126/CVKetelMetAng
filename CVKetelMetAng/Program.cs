8f66798faa4977b37480b2b902bf155dbcab5d3d
babacool126 <babacool126@gmail.com>
2024-02-07 21:49:20 +0100

    add modifications

--- CVKetelMetAng/Program.cs
+++ CVKetelMetAng/Program.cs
@@ -10,6 +10,18 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 
 
 builder.Services.AddControllers();
+
+// CORS configuration for localhost:4200
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy",
+        builder => builder
+            .WithOrigins("http://localhost:4200")
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .AllowCredentials());
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -24,6 +36,11 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseRouting();
+
+// Enable CORS
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
