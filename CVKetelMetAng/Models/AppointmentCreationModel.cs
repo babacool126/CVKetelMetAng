529f155376f1c090a740d5b38e812470084c3df5
babacool126 <babacool126@gmail.com>
2024-03-11 22:46:56 +0100

    error messages in French

--- CVKetelMetAng/Models/AppointmentCreationModel.cs
+++ CVKetelMetAng/Models/AppointmentCreationModel.cs
@@ -4,25 +4,25 @@ namespace CVKetelMetAng.Models
 {
     public class AppointmentCreationModel
     {
-        [Required(ErrorMessage = "The email address is required.")]
-        [EmailAddress(ErrorMessage = "Invalid Email Address.")]
-        [MaxLength(100, ErrorMessage = "The email address must be less than 100 characters.")]
+        [Required(ErrorMessage = "L'adresse e-mail est requise.")]
+        [EmailAddress(ErrorMessage = "L'adresse e-mail n'est pas valide.")]
+        [MaxLength(100, ErrorMessage = "L'adresse e-mail doit contenir moins de 100 caractères.")]
         public string CustomerEmail { get; set; }
 
-        [Required(ErrorMessage = "The name is required.")]
-        [MaxLength(50, ErrorMessage = "The name must be less than 50 characters.")]
+        [Required(ErrorMessage = "Le nom est requis.")]
+        [MaxLength(50, ErrorMessage = "Le nom doit contenir moins de 50 caractères.")]
         public string CustomerName { get; set; }
 
-        [MaxLength(20, ErrorMessage = "The phone number must be less than 20 characters.")]
+        [MaxLength(20, ErrorMessage = "Le numéro de téléphone doit contenir moins de 20 caractères.")]
         public string CustomerPhoneNumber { get; set; }
 
-        [MaxLength(200, ErrorMessage = "The address must be less than 200 characters.")]
+        [MaxLength(200, ErrorMessage = "L'adresse doit contenir moins de 200 caractères.")]
         public string CustomerAddress { get; set; }
 
-        [Required(ErrorMessage = "The appointment type is required.")]
+        [Required(ErrorMessage = "Le type de rendez-vous est requis.")]
         public SoortAfspraak AppointmentType { get; set; }
 
-        [Required(ErrorMessage = "The appointment date and time are required.")]
+        [Required(ErrorMessage = "La date et l'heure du rendez-vous sont requises.")]
         public DateTime AppointmentDateTime { get; set; }
     }
 }
